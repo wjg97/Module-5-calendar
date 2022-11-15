1e3439b4e6f26f32e5f618d1dd678f8a0cc03338
wjg97 <113846649+wjg97@users.noreply.github.com>
2022-11-14 17:13:40 -0700

    Updated HTML, CSS, JS

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,11 +1,11 @@
-// updates time on the webpage
+// setting the time in the header
 function updateTime() {
   let today = moment();
 
-  // updates the time element in the header
+  // time updates 
   $("#today").text(today.format("dddd, MMMM Do YYYY, h:mm.ss"));
 
-  // For coloring the past, present, and future time blocks
+  // time blocks for past, present, and future
   let now = moment().format("kk");
   for (let i = 0; i < scheduleElArray.length; i++) {
       scheduleElArray[i].removeClass("future past present");
@@ -48,27 +48,20 @@ let scheduleElArray = [
   schedule17,
 ];
 
-renderLastRegistered();
+renderLocalStorage();
 updateTime();
 setInterval(updateTime, 1000); 
 
-// render schedule saved in local storage
-function renderLastRegistered() {
+// puts schedule in local storage
+function renderLocalStorage() {
   for (let el of scheduleElArray) {
-      el.val(localStorage.getItem("time block " + el.data("hour")));
+      el.val(localStorage.getItem("current-time" + el.data("hour")));
 
   }
 }
 
-$('#clear-day').on('click',function(){
-  localStorage.clear();
-  //initPage()
-})
-console.log()
-
-
-// function for handling clicks
-function handleFormSubmit(event) {
+//save button function
+function saveToLocalStorage(event) {
   event.preventDefault();
 
   let btnClicked = $(event.currentTarget);
@@ -77,26 +70,7 @@ function handleFormSubmit(event) {
 
   let targetTimeBlock = targetText.data("hour");
 
-  localStorage.setItem("time block " +  targetTimeBlock, targetText.val());
+  localStorage.setItem("current-time" +  targetTimeBlock, targetText.val());
 }
 
-saveBttn.on("click", handleFormSubmit);
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
+saveBttn.on("click", saveToLocalStorage);
